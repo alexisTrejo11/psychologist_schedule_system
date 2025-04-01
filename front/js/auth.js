8b366d0522e03f47f6b157bffb3795ae1b680e41
Alexis Trejo <marcoalexispt.02@gmail.com>
2025-04-01 17:34:42 -0600

    Fix CORS

---
+++ front/js/auth.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById("loginForm")
+    const loginError = document.getElementById("loginError")
+    
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+    try {
+        console.log("Intentando iniciar sesión con:", { email, password });
+        
+        const response = await fetch("http://localhost:8000/login/", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        
+        console.log("Respuesta de la API:", response);
+        
+        if (response.ok) {
+            const data = await response.json();
+            console.log("Inicio de sesión exitoso:", data);
+            localStorage.setItem("access_token", data.access_token);
+            localStorage.setItem("refresh_token", data.refresh_token);
+        
+            window.location.href = "./home.html";
+        } else {
+            const errorData = await response.json();
+            console.error("Error en la API:", errorData);
+            loginError.textContent = errorData.message || "Correo electrónico o contraseña incorrectos.";
+            loginError.style.display = "block";
+        }
+        } catch (error) {
+        console.error("Error inesperado:", error);
+        loginError.textContent = "Ocurrió un error al intentar iniciar sesión. Por favor, inténtalo más tarde.";
+        loginError.style.display = "block";
+        }
+    });
+});
